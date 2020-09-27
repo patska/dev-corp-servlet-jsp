ee5f0ea6858537e84ebf36fd37180cac4f20a855
Lenon Cristhians <lenoncristhians@hotmail.com>
2020-09-26 23:53:17 -0300

    Tratamento de excessão NumberFormatException

--- terceiro-exercicio/src/main/java/edu/lenoncristhians/terceiro/servlet/INSSServlet.java
+++ terceiro-exercicio/src/main/java/edu/lenoncristhians/terceiro/servlet/INSSServlet.java
@@ -3,6 +3,9 @@ package edu.lenoncristhians.terceiro.servlet;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -26,32 +29,44 @@ public class INSSServlet extends HttpServlet {
         String jspDestination = "calculo.jsp";
         RequestDispatcher requestDispatcher = req.getRequestDispatcher(jspDestination);
         ArrayList<ArrayList<Double>> aliquot = brazilianINSSAliquotMap();
-        Double salary = stringToBRLConverter(req.getParameter("salary"));
-        Double netSalary = 0.0;
-        Double taxDiscount = 0.0;
+        Double salary = 0.00;
+        try{
+            salary = stringToBRLConverter(req.getParameter("salary"));
+        } catch(NumberFormatException numberFormatException){
+            logger.error("[NumberFormatException] Error when converting input to double. Redirecting to homepage...");
+            resp.sendRedirect("/terceiro-exercicio");
+        }
+        Double netSalary = 0.00;
+        Double taxDiscount = 0.00;
+        ArrayList<String> messages = new ArrayList<>();
+        int aliquotCurrentIndex = 1;
 
-        logger.info("[INSSServlet] Start...");
+        logger.info("[INSSServlet] INSSServlet Init.");
         logger.info("[INSSServlet] Input value: " + salary);
 
         for (ArrayList<Double> arrayList : aliquot) {
-            Double temp;
+            logger.info("[INSSServlet] aliquot current index: " + Precision.round(netSalary, 2));
+            Double temp = 0.00;
             if (salary > arrayList.get(1)) {
                 temp = Precision.round((arrayList.get(1) - arrayList.get(0)) * arrayList.get(2), 2);
                 taxDiscount += temp;
                 logger.info("[INSSServlet] First condition for temporary tax discount value: " + temp);
+                messages.add("- " + aliquotCurrentIndex + "ª Faixa Salarial: " +  brlToStringConverter(arrayList.get(1)) + " x " + arrayList.get(2) * 100 + "% = " + temp);
             }
             if (salary > arrayList.get(0) && salary <= arrayList.get(1)) {
                 temp = Precision.round((salary - arrayList.get(0)) * arrayList.get(2), 2);
                 taxDiscount += temp;
                 logger.info("[INSSServlet] Second condition for temporary tax discount value: " + temp);
-
+                messages.add("- " + aliquotCurrentIndex + "ª Faixa Salarial: " +  brlToStringConverter(arrayList.get(1)) + " x " + arrayList.get(2) * 100 + "% = " + temp);
             }
+            aliquotCurrentIndex++;
         }
-
+        
         netSalary = salary - taxDiscount;
         req.setAttribute("salary", brlToStringConverter(Precision.round(salary, 2)));
-        req.setAttribute("netSalary", Precision.round(netSalary, 2));
-        req.setAttribute("taxDiscount", Precision.round(taxDiscount, 2));
+        req.setAttribute("netSalary", brlToStringConverter(Precision.round(netSalary, 2)));
+        req.setAttribute("taxDiscount", brlToStringConverter(Precision.round(taxDiscount, 2)));
+        req.setAttribute("messages", messages);
         logger.info("[INSSServlet] netSalary total: " + Precision.round(netSalary, 2));
         logger.info("[INSSServlet] taxdiscount total: " + Precision.round(taxDiscount, 2));
         requestDispatcher.forward(req, resp);
@@ -76,6 +91,9 @@ public class INSSServlet extends HttpServlet {
     public String brlToStringConverter(Double value) {
         String st = "R$ " + Precision.round(value, 2);
         st = st.replace(".", ",");
+        if(st.split(",")[1].length() == 1){
+            st = st + "0";
+        }
         logger.info("[INSSServlet - brlToStringConverter] Return value: " + st);
         return st;
     }
